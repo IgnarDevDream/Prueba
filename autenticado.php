e920a3f921ce3474477581c80178898b925565e2
IgnarDevDream <igor.moreno.odz@gmail.com>
2025-03-02 23:05:14 +0100

    Modificando el inicio a pagina principal

--- autenticado.php
+++ autenticado.php
@@ -1,3 +1,26 @@
 <?php
-
+    session_start();
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['nombre']) && !empty($_POST['clave'])) {
+            $nombre = $_POST['nombre'];
+            $clave = $_POST['clave'];
+            
+            $_SESSION['nombreSesion'] = $nombre;
+            $_SESSION['claveSesion'] = '1234';
+            
+            // El código original compara la clave de sesión con la entrada del formulario sin verificar realmente la clave proporcionada.
+            // Es probable que quieras tener una verificación adecuada de la clave en función de tu lógica de autenticación.
+            
+            if ($_SESSION['nombreSesion'] == $nombre && $_SESSION['claveSesion'] == $clave) {
+                header('Location: pagina_principal.php');
+                exit();
+            } else {
+                header('Location: index.html');
+                exit();
+            }
+        } else {
+            echo "Por favor, ingresa los datos";
+        }
+    }
 ?>
--- pagina_principal.php
+++ pagina_principal.php
@@ -10,7 +10,7 @@
 	</head>
 	<body>
 		<?php
-			echo "hola"
+			echo "hola a todos"
 		?>
 	</body>
 </html>
