efac831b5e8a2d45a7cc2617c610d68357177863
IgnarDevDream <igor.moreno.odz@gmail.com>
2025-03-02 22:16:08 +0100

    Probando

---
+++ inicio.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>Examen</title>
+		<style>
+			*{
+				background-color: aqua;
+			}
+			</style>
+	</head>
+	<body>
+		<?php
+			echo "hola"
+		?>
+	</body>
+</html>
